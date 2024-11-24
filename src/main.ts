bf094fe8f7607577aaf37b034d682def73176ae0
Tomáš Sabol <tomassabol2@gmail.com>
2024-11-24 09:45:10 +0100

    Fix deployment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./util/env";
 
 import { NestFactory } from "@nestjs/core";
-import * as cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 import { AppModule } from "./app.module";
 
