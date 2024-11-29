a5c75b346b9a442c2c4fda9029f7ef4a2278860d
Tomáš Sabol <tomassabol2@gmail.com>
2024-11-29 09:50:07 +0100

    Fix supertest import

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,7 +1,7 @@
 import type { INestApplication } from "@nestjs/common";
 import { Test, type TestingModule } from "@nestjs/testing";
 import type { Server } from "http";
-import * as request from "supertest";
+import request from "supertest";
 
 import { AuthService } from "../src/(routes)/auth/auth.service";
 import type {
--- test/ensembles.e2e-spec.ts
+++ test/ensembles.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { type INestApplication } from "@nestjs/common";
 import { Test, type TestingModule } from "@nestjs/testing";
 import { type Server } from "http";
-import * as request from "supertest";
+import request from "supertest";
 
 import { EnsemblesService } from "../src/(routes)/ensembles/ensembles.service";
 import type { CreateEnsembleBody } from "../src/(routes)/ensembles/lib/validation-schemas";
